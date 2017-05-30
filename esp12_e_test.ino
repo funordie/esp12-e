349ca7c840c6415440631aa8305b7bfd85bc3965
Iliya Paev <iliapaev@gmail.com>
2017-05-30 23:36:31 +0300

    manager: use autoconnect

--- esp12_e_test.ino
+++ esp12_e_test.ino
@@ -55,24 +55,9 @@ void setup() {
   //Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
 
-  //reset settings - for testing
-  //wifiManager.resetSettings();
-
-  //sets timeout until configuration portal gets turned off
-  //useful to make it all retry or go to sleep
-  //in seconds
-  //wifiManager.setTimeout(120);
-
-  //it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
-
-  //WITHOUT THIS THE AP DOES NOT SEEM TO WORK PROPERLY WITH SDK 1.5 , update to at least 1.5.1
-  //WiFi.mode(WIFI_STA);
-
   String ssid = "THERM" + String(ESP.getChipId());
   delay(3000);
-  if (!wifiManager.startConfigPortal(ssid.c_str())) {
+  if (!wifiManager.autoConnect(ssid.c_str())) {
     Serial.println("failed to connect and hit timeout");
     delay(3000);
     //reset and try again, or maybe put it to deep sleep
