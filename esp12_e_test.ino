8f9b0ca60a78a5734f3e18b874497b8d1a94f4cf
Iliya Paev <iliapaev@gmail.com>
2017-05-30 23:23:15 +0300

    add mqtt test server

--- esp12_e_test.ino
+++ esp12_e_test.ino
@@ -5,7 +5,47 @@
 #include <DNSServer.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
+#include <PubSubClient.h>
+WiFiClient espClient;
+PubSubClient client(espClient);
+const char* mqtt_server = "broker.mqttdashboard.com";
+const byte ledPin = 13; // Pin with LED
 
+void callback(char* topic, byte* payload, unsigned int length) {
+ Serial.print("Message arrived [");
+ Serial.print(topic);
+ Serial.print("] ");
+ for (int i=0;i<length;i++) {
+  char receivedChar = (char)payload[i];
+  Serial.print(receivedChar);
+  if (receivedChar == '0')
+  // ESP8266 Huzzah outputs are "reversed"
+  digitalWrite(ledPin, HIGH);
+  if (receivedChar == '1')
+   digitalWrite(ledPin, LOW);
+  }
+  Serial.println();
+}
+
+
+void reconnect() {
+ // Loop until we're reconnected
+ while (!client.connected()) {
+ Serial.print("Attempting MQTT connection...");
+ // Attempt to connect
+ if (client.connect("ESP8266 Client")) {
+  Serial.println("connected");
+  // ... and subscribe to topic
+  client.subscribe("ledStatus");
+ } else {
+  Serial.print("failed, rc=");
+  Serial.print(client.state());
+  Serial.println(" try again in 5 seconds");
+  // Wait 5 seconds before retrying
+  delay(5000);
+  }
+ }
+}
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -42,10 +82,18 @@ void setup() {
 
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  pinMode(ledPin, OUTPUT);
+  Serial.println("exit setup");
 }
 
 void loop() {
 
   // put your main code here, to run repeatedly:
-
+	 if (!client.connected()) {
+	  reconnect();
+	 }
+	 client.loop();
 }
