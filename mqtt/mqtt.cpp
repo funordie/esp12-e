f81142112b4792c32d5c592f06a4d3fd79425a39
Iliya Paev <iliapaev@gmail.com>
2017-06-22 00:00:22 +0300

    mqtt: add OnTime topic

--- mqtt/mqtt.cpp
+++ mqtt/mqtt.cpp
@@ -17,19 +17,35 @@ static WiFiClient espClient;
 static PubSubClient client(espClient);
 static const char* mqtt_server = "broker.mqttdashboard.com";
 static const byte ledPin = 5; // Pin with LED
-uint32_t chip_id;
+static uint32_t chip_id;
+static uint32_t OnTime;
 
+static String topic_OnTime;
+static String topic_LedStatus;
 void callback(char* topic, byte* payload, unsigned int length) {
 	Serial.print("Message arrived [");
 	Serial.print(topic);
 	Serial.print("] ");
-	for (unsigned int i=0;i<length;i++) {
-		char receivedChar = (char)payload[i];
-		Serial.print(receivedChar);
-		if (receivedChar == '0')
-			digitalWrite(ledPin, LOW);
-		if (receivedChar == '1')
-			digitalWrite(ledPin, HIGH);
+
+	if(!strcmp(topic_OnTime.c_str(), topic)) {
+		//received topic OnTime
+		String str;
+		for (unsigned int i=0;i<length;i++) {
+			str += (char)payload[i];
+		}
+		Serial.println(str);
+		OnTime = str.toInt();
+	}
+	else if(!strcmp(topic_LedStatus.c_str(), topic)) {
+		//received topic LedStatus
+		for (unsigned int i=0;i<length;i++) {
+			char receivedChar = (char)payload[i];
+			Serial.print(receivedChar);
+			if (receivedChar == '0')
+				digitalWrite(ledPin, LOW);
+			if (receivedChar == '1')
+				digitalWrite(ledPin, HIGH);
+		}
 	}
 	Serial.println();
 }
@@ -41,10 +57,12 @@ void reconnect() {
 		// Attempt to connect
 		if (client.connect("ESP8266 Client")) {
 			Serial.println("connected");
-			// ... and subscribe to topic
-			String str = String(chip_id) + "_" + "ledStatus";
-			Serial.println("subscribe to:" + str);
-			client.subscribe(str.c_str());
+
+			Serial.println("subscribe to:" + topic_LedStatus);
+			client.subscribe(topic_LedStatus.c_str());
+
+			Serial.println("subscribe to:" + topic_OnTime);
+			client.subscribe(topic_OnTime.c_str());
 		} else {
 			Serial.print("failed, rc=");
 			Serial.print(client.state());
@@ -58,6 +76,9 @@ void reconnect() {
 void setup_mqtt() {
 	EspClass esp;
 	chip_id = esp.getChipId();
+
+	topic_LedStatus = String(chip_id) + "_" + "ledStatus";
+	topic_OnTime = String(chip_id) + "_" + "OnTime";
 
 	//mqtt server start
 	client.setServer(mqtt_server, 1883);
